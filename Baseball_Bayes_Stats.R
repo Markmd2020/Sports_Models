20644a92cf2b7ef784a7a4d87ff379a045fa624b
Mark Montanana <markm8681@hotmail.com>
2026-01-30 20:29:52 +0000

    Initial simulation example

---
+++ Baseball_Bayes_Stats.R
@@ -0,0 +1,11 @@
+######Empirical  Bayes Examples For Basebal####
+
+#Load libraries
+library(dplyr)
+
+#Simulation of hits examples
+num_trials <- 10e6
+simulations <- data_frame(
+  true_average = rbeta(num_trials, 81, 219),
+  hits = rbinom(num_trials, 300, true_average)
+)
